c85e7a98a9c16a899daecc137aed550f54762c84
Cesar-Quintanilla <quintanilla.cesar@ymail.com>
2022-11-16 00:13:58 -0800

    finished functionality on components for post and get requests

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,31 +11,11 @@ const Navbar = ({ username, userId }) => {
     setShowAddTaskModal(!showAddTaskModal);
   };
 
-  const userToDoListforThisDate = async (e) => {
-    e.preventDefault();
-    const body = { userId: userId, date: e.target.date.value };
-    try {
-      //NEED CORRECT ENDPOINT
-      fetch('/api/trask', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: JSON.stringify(body),
-      });
-      const data = JSON.stringify(body);
-      // name of the todolist
-      setInputText(body.name);
-      //assume body is an object
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
 
   return (
     <nav className='navbar'>
       <div className='leftNav'>
-        <input type='date' id='date' name='date'></input>
+        <input type='date' id='date' name='date' ></input>
       </div>
 
       <div className='rightNav'>
@@ -43,6 +23,7 @@ const Navbar = ({ username, userId }) => {
         <Modal
           showAddTaskModal={showAddTaskModal}
           toggleAddTaskModal={toggleAddTaskModal}
+          userId={userId}
         />
       </div>
     </nav>
--- client/src/components/TodoInput.js
+++ client/src/components/TodoInput.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Modal from './AddTaskModal';
 
-const TodoInputBox = () => {
-  return <div></div>;
+const TodoInputBox = ({userId, data}) => {
+
+  const [showAddTaskModal, setShowAddTaskModal] = useState(false);
+
+  const toggleAddTaskModal = () => {
+    setShowAddTaskModal(!showAddTaskModal);
+  };
+
+  const arr = [];
+  // assuming that data is an array of objects
+  data?.map((obj) => {
+    
+    //CHECK THE DATA FROM THE OBJECT ********************************
+    if (obj.date && !obj.time){
+      let chunk = 
+          <li> 
+            {obj.name}
+            <button onClick={toggleAddTaskModal}>Update</button>
+            <Modal
+              showAddTaskModal={showAddTaskModal}
+              toggleAddTaskModal={toggleAddTaskModal}
+            />
+          </li>;
+      arr.push(chunk);
+    }
+  });
+  
+  return (
+  <div>
+       <div className='rightNav'>
+        <ul className='ul'>
+          {arr}
+        </ul>
+      </div>
+  </div>
+  );
 };
 
 export default TodoInputBox;
