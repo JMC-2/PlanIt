d085ab60267fc90dd75744b2f48369e6e0540dfc
Johnjered <johnjeredtolentino@gmail.com>
2022-11-16 21:13:59 -0800

    Styling revamp 3.0

--- client/src/data/hours.js
+++ client/src/data/hours.js
@@ -1,55 +1,67 @@
- const hoursDetail = [
-    {
-        time:'8:00 AM'
-    },
-    {
-        time:'9:00 AM'
-    },
-    {
-        time:'10:00 AM'
-    },
-    {
-        time:'11:00 AM'
-    },
-    {
-        time:'12:00 PM'
-    },
-    {
-        time:'1:00 PM'
-    },
-    {
-        time:'2:00 PM'
-    },
-    {
-        time:'3:00 PM'
-    },
-    {
-        time:'4:00 PM'
-    },
-    {
-        time:'5:00 PM'
-    },
-    {
-        time:'6:00 PM'
-    },
-    {
-        time:'7:00 PM'
-    },
-    {
-        time:'8:00 PM'
-    },
-    {
-        time:'9:00 PM'
-    },
-    {
-        time:'10:00 PM'
-    },
-    {
-        time:'11:00 PM'
-    },
-    {
-        time:'12:00 AM'
-    },
+const hoursDetail = [
+  {
+    time:'4:00 AM'
+  },
+  {
+    time:'5:00 AM'
+  },
+  {
+    time:'6:00 AM'
+  },
+  {
+    time:'7:00 AM'
+  },
+  {
+    time:'8:00 AM'
+  },
+  {
+    time:'9:00 AM'
+  },
+  {
+    time:'10:00 AM'
+  },
+  {
+    time:'11:00 AM'
+  },
+  {
+    time:'12:00 PM'
+  },
+  {
+    time:'1:00 PM'
+  },
+  {
+    time:'2:00 PM'
+  },
+  {
+    time:'3:00 PM'
+  },
+  {
+    time:'4:00 PM'
+  },
+  {
+    time:'5:00 PM'
+  },
+  {
+    time:'6:00 PM'
+  },
+  {
+    time:'7:00 PM'
+  },
+  {
+    time:'8:00 PM'
+  },
+  {
+    time:'9:00 PM'
+  },
+  {
+    time:'10:00 PM'
+  },
+  {
+    time:'11:00 PM'
+  },
+  {
+    time:'12:00 AM'
+  },
 ]
 
 export default hoursDetail;
